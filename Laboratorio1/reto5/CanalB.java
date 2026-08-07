12bfe834d2c243b309694225f2d6fe5cdf01f57c
JulianMorales2003 <julian.morales-z@mail.escuelaing.edu.co>
2026-08-07 18:51:19 -0500

    Agrego solucion del reto 5

--- Laboratorio1/reto5/CanalB.java
+++ Laboratorio1/reto5/CanalB.java
@@ -1,5 +1,32 @@
 package Laboratorio1.reto5;
 
+import java.util.Scanner;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
 public class CanalB {
-    
+
+    public static TreeSet<Integer> filtrarMultiplosDe5(TreeSet<Integer> numeros) {
+        return numeros.stream()
+                .filter(n -> n % 5 != 0)
+                .collect(Collectors.toCollection(TreeSet::new));
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        TreeSet<Integer> treeSet = new TreeSet<>();
+
+        System.out.print("Ingrese los números del TreeSet separados por espacio: ");
+        String linea = scanner.nextLine();
+
+        for (String parte : linea.trim().split("\\s+")) {
+            treeSet.add(Integer.parseInt(parte));
+        }
+
+        TreeSet<Integer> resultado = filtrarMultiplosDe5(treeSet);
+
+        resultado.forEach(n -> System.out.println("Número en arena: " + n));
+
+        scanner.close();
+    }
 }
